1805201408267866107f312f7b52b0e6f2ea9cb6
sachin saw <114833574+sachinsaw924@users.noreply.github.com>
2024-10-06 04:50:02 +0530

    Update 46_permutation.cpp

--- Backtracking/46_permutation.cpp
+++ Backtracking/46_permutation.cpp
@@ -1,3 +1,8 @@
+// approach 1 : Revise once again this problem 
+// Revision mark : 0
+// This is solve by general bracktracking method khandani template 
+
+
 class Solution {
 public:
   
@@ -31,5 +36,41 @@ public:
         solve(temp,nums,result,st);
         return result;
  
+    }
+};
+
+
+//now swap methoding used
+// make tree diagram again while revising 
+// Revision mark : 0
+
+
+
+
+
+class Solution {
+public:
+      vector<vector<int>>result;
+      int n ;
+      void solve(int idx,vector<int>& nums)
+      {
+        if(idx == n )
+        {
+            result.push_back(nums);
+            return  ;
+        }
+
+        for(int i = idx ;i < n ; i++)
+        {
+            swap(nums[i],nums[idx]);
+            solve(idx+1,nums);
+            swap(nums[i],nums[idx]);
+        }
+      }
+    vector<vector<int>> permute(vector<int>& nums) {
+        int i = 0;
+        n = nums.size();
+        solve(i,nums);
+        return result;
     }
 };

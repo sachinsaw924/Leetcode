6956ee481f06f955e0dbf308d5eb804b2aa7ecab
sachin saw <114833574+sachinsaw924@users.noreply.github.com>
2024-10-06 04:17:54 +0530

    Update 46_permutation.cpp

--- Backtracking/46_permutation.cpp
+++ Backtracking/46_permutation.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+  
+  void solve(vector<int>&temp,vector<int>& nums, vector<vector<int>>&result,unordered_set<int>&st)
+  { 
 
+     
+     if(temp.size()==nums.size())
+      {
+        result.push_back(temp);
+        return ;
+      }
+       for(int i = 0 ;i<nums.size();i++)
+        {
+            if(st.find(nums[i])==st.end())
+            {   temp.push_back(nums[i]);
+                st.insert(nums[i]);
+                solve(temp,nums,result,st);
+                temp.pop_back();
+                st.erase(nums[i]);
+
+
+            }
+        }
+  }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>result;
+        vector<int>temp;
+        unordered_set<int>st;
+        solve(temp,nums,result,st);
+        return result;
+ 
+    }
+};

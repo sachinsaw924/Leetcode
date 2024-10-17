1875ca5068fabb9cf2580180c61d47e9dd81c0df
sachin saw <114833574+sachinsaw924@users.noreply.github.com>
2024-10-17 20:59:28 +0530

    Update 670. Maximum Swap.cpp

--- Array/PrefixRelated/670. Maximum Swap.cpp
+++ Array/PrefixRelated/670. Maximum Swap.cpp
@@ -33,3 +33,55 @@ public:
         return num;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bhaii dekh isko ro revise krna hi parega bina revise kiye krna muskil hi isko 
+// isme jo maxRight ke ander jo access kr rhe hai wo thora difficult hai km se km 5 -6  dry run kr lena 
+// yanha pr time complexity or space complexity dono reduce ho gya hai 
+
+
+
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string s = to_string(num);
+        int n = s.length();
+
+        vector<int>maxRight(10,-1) ; // 0,1,2,.......10
+
+        for(int i = 0 ;i<n;i++)
+        {
+            int currChar = s[i];
+            int currDigit = s[i]-'0';
+            maxRight[currDigit] = i ;
+        }
+
+        for(int i =  0 ;i<n;i++)
+        {
+            int currChar = s[i];
+            int currDigit = s[i]-'0';
+
+            for(int digit = 9 ; digit>currDigit ; digit --)
+            {
+                if(maxRight[digit]>i)
+                {
+                    swap(s[i],s[maxRight[digit]]);
+                    return stoi(s);
+                }
+
+            }
+        }
+        return num ;
+    }
+};

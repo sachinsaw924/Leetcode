9863874598a4eb8ecb1b831585324a7e79c519a6
sachin saw <114833574+sachinsaw924@users.noreply.github.com>
2024-11-11 00:57:07 +0530

    Create 3097. Shortest Subarray With OR at Least K II.cpp

---
+++ BitManipulation/3097. Shortest Subarray With OR at Least K II.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+   // ikso mughe revise krna parega ek do bar wapas se wrna concept dhayan me nahi rahega 
+    void update(int number,vector<int>&vec,int val)
+    {
+        for(int i = 0;i<32;i++)
+        {
+             if((number>>i) & 1)
+             {
+                vec[i] += val;
+            }
+        }
+    }
+     
+    int getDecimalFromBinary(vector<int>&vec)
+    {  int num = 0 ;
+        for(int i = 0 ;i<32;i++)
+       {
+            if(vec[i] > 0) {
+                num |= (1 << i);
+            }
+         }
+       
+        return num;
+    }
+     
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+       int i = 0 ;
+       int j = 0 ;
+       vector<int>vec(32,0);
+       int result = INT_MAX;
+
+       int n = nums.size();
+
+       while(j<n)
+       {
+        update(nums[j],vec,1);
+
+        while(i<=j && getDecimalFromBinary(vec) >=k)
+        {
+            result = min(result,j - i + 1);
+            update(nums[i],vec,-1);
+            i++;
+        }
+         j++;
+       } 
+      return result == INT_MAX ? -1 : result;
+    }
+};

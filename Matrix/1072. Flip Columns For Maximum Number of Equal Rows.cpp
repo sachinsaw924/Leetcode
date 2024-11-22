441e51d6ee64dae11e9f255965aebdcbadfbfeb8
sachin saw <114833574+sachinsaw924@users.noreply.github.com>
2024-11-22 16:14:02 +0530

    Update 1072. Flip Columns For Maximum Number of Equal Rows.cpp

--- Matrix/1072. Flip Columns For Maximum Number of Equal Rows.cpp
+++ Matrix/1072. Flip Columns For Maximum Number of Equal Rows.cpp
@@ -34,3 +34,42 @@ public:
 
     }
 };
+
+
+
+
+
+// thora effiecient hai pehle wale question se lekin space hai wo jyada le rhe hai 
+// observing question ache se observe kre to ho jaega 
+
+
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int maxValues = 0 ;
+
+
+        unordered_map<string,int>mp; // O(m*n)
+
+        for(auto &row : matrix) // O(m)
+        {
+            string rawKaNature = "";
+            int value = row[0];
+
+            for(int col = 0 ; col < n ;col++) // O(n)
+            {
+                rawKaNature +=(row[col] == value) ? "S" : "B";
+            }
+            mp[rawKaNature]++;
+
+        }
+           for(auto &it : mp)
+            {
+                maxValues = max(maxValues , it.second);
+            }
+            return maxValues ;
+
+   }
+};

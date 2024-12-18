3338221afedd28fe430162f4bc1eeef093a19e44
sachin saw <114833574+sachinsaw924@users.noreply.github.com>
2024-12-19 02:57:21 +0530

    Update 1475. Final Prices With a Special Discount in a Shop.cpp

--- MonotonicStack/1475. Final Prices With a Special Discount in a Shop.cpp
+++ MonotonicStack/1475. Final Prices With a Special Discount in a Shop.cpp
@@ -25,3 +25,27 @@ public:
         return result;
     }
 };
+
+
+
+// done by myself
+
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+
+        for(int i= 0 ; i < n ; i++)
+        {
+            for(int j = i+1 ; j < n ;j++)
+            {
+                if(prices[j]<=prices[i])
+                {
+                    prices[i] = prices[i]-prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+};

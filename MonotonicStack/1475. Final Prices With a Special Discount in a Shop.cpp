d2a8697969107577bcf1b26d16734c9bc7e808c1
sachin saw <114833574+sachinsaw924@users.noreply.github.com>
2024-12-19 02:54:40 +0530

    Create 1475. Final Prices With a Special Discount in a Shop.cpp

---
+++ MonotonicStack/1475. Final Prices With a Special Discount in a Shop.cpp
@@ -0,0 +1,24 @@
+
+
+// Tc O(2*n)  // do bar hi visit kr rhe hai
+// sc o(n )
+
+
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        vector<int>result(begin(prices),end(prices));
+        stack<int>st;
+        for(int i = 0 ; i < n  ; i++)
+        {
+            while(!st.empty() && prices[i] <= prices[st.top()])
+            {
+                result[st.top()] = result[st.top()]-prices[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return result;
+    }
+};

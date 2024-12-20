b57e34b4a2a80aa0171e283e68356eec799ff685
sachin saw <114833574+sachinsaw924@users.noreply.github.com>
2024-12-21 03:05:40 +0530

    Update 2415. Reverse Odd Levels of Binary Tree.cpp

--- BinaryTree/2415. Reverse Odd Levels of Binary Tree.cpp
+++ BinaryTree/2415. Reverse Odd Levels of Binary Tree.cpp
@@ -12,6 +12,8 @@
  * right(right) {}
  * };
  */
+
+// BFS
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
@@ -60,5 +62,39 @@ public:
         }
 
         return root;
+    }
+};
+
+
+
+
+// DFS
+
+
+
+class Solution {
+public:
+
+   void solve(TreeNode* L , TreeNode* R , int level)
+   {
+    if(L == nullptr || R == nullptr)
+    {
+        return ;
+    }
+
+    if(level %2 ==1)
+    {
+        int temp = L->val;
+        L->val = R->val;
+        R->val = temp;
+    }
+
+    solve(L->left,R->right,level+1);
+    solve(L->right,R->left , level + 1);
+
+   }
+    TreeNode* reverseOddLevels(TreeNode* root) {
+         solve(root->left , root->right , 1);
+         return root ;
     }
 };

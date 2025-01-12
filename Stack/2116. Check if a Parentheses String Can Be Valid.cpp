42609feb87acc5fcf94b5df4a75e68cd3f1953ae
sachin saw <114833574+sachinsaw924@users.noreply.github.com>
2025-01-12 20:46:24 +0530

    Update 2116. Check if a Parentheses String Can Be Valid.cpp

--- Stack/2116. Check if a Parentheses String Can Be Valid.cpp
+++ Stack/2116. Check if a Parentheses String Can Be Valid.cpp
@@ -54,3 +54,59 @@ public:
 
     }
 };
+
+
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        
+
+        // ye approach to badhiya hai pr isme two pass lag rha hai
+        int n = s.size();
+        if(n%2 !=0)
+        {
+            return false;
+        }
+
+        int open = 0 ; 
+
+        for(int i = 0 ; i < n ;i++)
+        {
+            if(s[i] == '(' || locked[i] == '0')
+            {
+                open++;
+            }
+            else
+            {
+                open--;
+            }
+
+            if(open<0)
+            {
+                return false;
+            }
+        }
+
+
+         int close = 0 ; 
+
+        for(int i = n-1 ; i>=0 ;i--)
+        {
+            if(s[i] == ')' || locked[i] == '0')
+            {
+                close++;
+            }
+            else
+            {
+                close--;
+            }
+
+            if(close<0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};

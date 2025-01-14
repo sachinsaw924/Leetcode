8b393cb1730d824ed0f23d962807b01400acc958
sachin saw <114833574+sachinsaw924@users.noreply.github.com>
2025-01-14 17:30:12 +0530

    Update 2657. Find the Prefix Common Array of Two Arrays.cpp

--- Array/PrefixRelated/2657. Find the Prefix Common Array of Two Arrays.cpp
+++ Array/PrefixRelated/2657. Find the Prefix Common Array of Two Arrays.cpp
@@ -37,3 +37,85 @@ public:
         return result;
     }
 };
+
+
+/// by sir 
+
+
+// time complexity O(n*n*n) 
+// space complexity O(1)
+
+
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        vector<int>result;
+        int count = 0 ; 
+        int n = A.size();
+
+
+        for(int i = 0 ; i < n ; i++)
+        { 
+            count = 0 ;
+            for(int A_i = 0 ; A_i <=i ; A_i++)
+            {   
+                for(int B_i = 0 ; B_i<=i ; B_i++)
+                {
+                    if(A[A_i] == B[B_i])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            result.push_back(count);
+        }
+
+        return result;
+    }
+};
+
+
+
+
+
+
+//   by sir 
+// time complexity O(n)
+// space complexity O(n)
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        unordered_map<int,int>mp;
+        int n = A.size();
+        vector<int>result(n);
+
+          
+        int count = 0 ;
+
+        for(int i = 0 ; i < n ; i++)
+        {
+
+            mp[A[i]]++;
+
+            if(mp[A[i]] == 2)
+            {
+                count++;
+            }
+
+            mp[B[i]]++;
+            if(mp[B[i]] == 2)
+            {
+                count++;
+            }
+
+            result[i] =  count;
+        }
+
+        return result;
+    }
+};
+
+// time complexity O(n)
+// space complexity O(n)
+
